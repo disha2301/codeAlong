2ec2eb01d322344010a1e7e611ce628174c8b67a
Disha <dishay166@gmail.com>
2023-08-25 23:18:22 +0530

    add pattern

--- 6.cpp
+++ 6.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void pattern4(int num){
-   for(int i=0;i<num;i++){
-    for(int j=i;j<=num;j++){
-        cout<<"* ";
+   for(int i=1;i<=num;i++){
+    for(int j=1;j<=num-i+1;j++){
+        cout<<j<<" ";
     }
     cout<<endl;
    }
